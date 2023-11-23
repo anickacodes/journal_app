537b61dc2520670afd9c1d1fbee66dd87406f7d7
anickacodes <anickalewis@pursuit.org>
2023-11-22 19:44:10 -0500

    removed unused

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import EntryList from "./components/EntryList";
-import EntryForm from "./components/EntryForm";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import Entry from "./components/Entry";
@@ -14,9 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<Entry />} />
-
           <Route path="/list" element={<EntryList />} />
-          {/* <Route path='/list/:id' element={<Entry/>}/> */}
         </Routes>
       </Router>
     </>
