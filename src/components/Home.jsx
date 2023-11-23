80ebaf88d36331f9d4d6e78fd6b318b0458e7cc0
anickacodes <anickalewis@pursuit.org>
2023-11-23 10:06:34 -0500

    styling, alignment

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,116 @@
-import '../styles/Home.css'
-import psalm from '../assets/91kjvold.jpeg'
+import "../styles/Home.css";
+import psalm from "../assets/91kjvold.jpeg";
 
-const Home = ( ) => {
+const Home = () => {
+  return (
+    <div className="home_container">
+      <img
+        id="psalm"
+        src={`${psalm}`}
+        alt="91 He that dwelleth in the secret place of the most High shall abide under the shadow of the Almighty.
 
-    return (
-        <div className="home_container">
-            <img id="psalm" src={`${psalm}`} alt='kjv 91 psalm'/>
-            <article>
+2 I will say of the Lord, He is my refuge and my fortress: my God; in him will I trust.
 
-            </article>
-        </div>
-    )
-}
+3 Surely he shall deliver thee from the snare of the fowler, and from the noisome pestilence.
 
-export default Home
+4 He shall cover thee with his feathers, and under his wings shalt thou trust: his truth shall be thy shield and buckler.
+
+5 Thou shalt not be afraid for the terror by night; nor for the arrow that flieth by day;
+
+6 Nor for the pestilence that walketh in darkness; nor for the destruction that wasteth at noonday.
+
+7 A thousand shall fall at thy side, and ten thousand at thy right hand; but it shall not come nigh thee.
+
+8 Only with thine eyes shalt thou behold and see the reward of the wicked.
+
+9 Because thou hast made the Lord, which is my refuge, even the most High, thy habitation;
+
+10 There shall no evil befall thee, neither shall any plague come nigh thy dwelling.
+
+11 For he shall give his angels charge over thee, to keep thee in all thy ways.
+
+12 They shall bear thee up in their hands, lest thou dash thy foot against a stone.
+
+13 Thou shalt tread upon the lion and adder: the young lion and the dragon shalt thou trample under feet.
+
+14 Because he hath set his love upon me, therefore will I deliver him: I will set him on high, because he hath known my name.
+
+15 He shall call upon me, and I will answer him: I will be with him in trouble; I will deliver him, and honour him.
+
+16 With long life will I satisfy him, and shew him my salvation."
+      />
+      <article>
+        <p>
+          <span className="verse-number">1</span> He that dwelleth in the secret
+          place of the most High shall abide under the shadow of the Almighty.
+        </p>
+        <p>
+          <span className="verse-number">2</span> I will say of the Lord, He is my
+          refuge and my fortress: my God; in him will I trust.
+        </p>
+        <p>
+          <span className="verse-number">3</span> Surely he shall deliver thee from
+          the snare of the fowler, and from the noisome pestilence.
+        </p>
+        <p>
+          <span className="verse-number">4</span> He shall cover thee with his
+          feathers, and under his wings shalt thou trust: his truth shall be thy
+          shield and buckler.
+        </p>
+        <p>
+          <span className="verse-number">5</span> Thou shalt not be afraid for the
+          terror by night; nor for the arrow that flieth by day;
+        </p>
+        <p>
+          <span className="verse-number">6</span> Nor for the pestilence that
+          walketh in darkness; nor for the destruction that wasteth at noonday.
+        </p>
+        <p>
+          <span className="verse-number">7</span> A thousand shall fall at thy side,
+          and ten thousand at thy right hand; but it shall not come nigh thee.
+        </p>
+        <p>
+          <span className="verse-number">8</span> Only with thine eyes shalt thou
+          behold and see the reward of the wicked.
+        </p>
+        <p>
+          <span className="verse-number">9</span> Because thou hast made the Lord,
+          which is my refuge, even the most High, thy habitation;
+        </p>
+        <p>
+          <span className="verse-number">10</span> There shall no evil befall thee,
+          neither shall any plague come nigh thy dwelling.
+        </p>
+        <p>
+          <span className="verse-number">11</span> For he shall give his angels
+          charge over thee, to keep thee in all thy ways.
+        </p>
+        <p>
+          <span className="verse-number">12</span> They shall bear thee up in their
+          hands, lest thou dash thy foot against a stone.
+        </p>
+        <p>
+          <span className="verse-number">13</span> Thou shalt tread upon the lion
+          and adder: the young lion and the dragon shalt thou trample under
+          feet.
+        </p>
+        <p>
+          <span className="verse-number">14</span> Because he hath set his love upon
+          me, therefore will I deliver him: I will set him on high, because he
+          hath known my name.
+        </p>
+        <p>
+          <span className="verse-number">15</span> He shall call upon me, and I will
+          answer him: I will be with him in trouble; I will deliver him, and
+          honour him.
+        </p>
+        <p>
+          <span className="verse-number">16</span> With long life will I satisfy
+          him, and shew him my salvation.
+        </p>
+      </article>
+    </div>
+  );
+};
+
+export default Home;
