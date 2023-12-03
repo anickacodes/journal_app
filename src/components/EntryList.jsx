e55500f71b282b06a49f67aa359dfd5c359fb7ba
anickacodes <anickalewis@pursuit.org>
2023-12-03 02:03:45 -0500

    static fetch ✅, flip effect, ts visuals

--- src/components/EntryList.jsx
+++ src/components/EntryList.jsx
@@ -1,11 +1,14 @@
 import { useLocation } from "react-router-dom";
 import "../styles/EntryList.css";
 import { useEffect, useState } from "react";
+import { useSpring, animated } from "react-spring";
+import { useGesture } from "react-use-gesture";
 
 const EntryList = () => {
   const location = useLocation();
-//   const { newEntry } = location.state || {};
+  //   const { newEntry } = location.state || {};
   const [entries, setEntries] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
     fetchEntries();
@@ -17,32 +20,81 @@ const EntryList = () => {
   };
 
   const handleDeleteEntry = (index) => {
-    const shouldDelete = window.confirm("Are you sure you want to delete this entry?");
+    const shouldDelete = window.confirm(
+      "Are you sure you want to delete this entry?"
+    );
     if (shouldDelete) {
-    const updatedEntries = [...entries];
-    updatedEntries.splice(index, 1);
-    setEntries(updatedEntries);
-    localStorage.setItem("entries", JSON.stringify(updatedEntries));
-  } else {
-  }
-  }
+      const updatedEntries = [...entries];
+      updatedEntries.splice(index, 1);
+      setEntries(updatedEntries);
+      localStorage.setItem("entries", JSON.stringify(updatedEntries));
+
+      // Adjust the current index after deleting an entry
+      setCurrentIndex(Math.min(currentIndex, updatedEntries.length - 1));
+    }
+  };
+  const bind = useGesture({
+    onDrag: ({ movement: [mx], down, direction: [xDir], velocity }) => {
+      const trigger = velocity > 0.2;
+
+      if (!down && trigger) {
+        xDir > 0 ? handlePrev() : handleNext();
+      }
+    },
+  });
+
+  const handleNext = () => {
+    setCurrentIndex((prev) => Math.min(prev + 1, entries.length - 1));
+  };
+
+  const handlePrev = () => {
+    setCurrentIndex((prev) => Math.max(prev - 1, 0));
+  };
+
+  const entryProps = useSpring({
+    transform: `translateX(${-currentIndex * 100}%)`,
+  });
 
   return (
     <div className="entriesContainer">
       <h1>Journal Entry List 📜</h1>
       <ol reversed>
-        {entries.map((entry, index) => (
-          <div key={index} className="entryCard">
-            <li key={index}>
-              <p>Date: {entry.date}</p>
-              <p>Content: {entry.content}</p>
-              <p>Time: {entry.currentTime}</p>
-              <p>Author: {entry.author || "Anonymous"}</p>
-            </li>
-            <button onClick={() => handleDeleteEntry(index)}>Delete Entry</button>
-          </div>
-        ))}
+        <div className="book" {...bind()}>
+          {entries.map((entry, index) => (
+            <animated.div className="entryWrapper" style={entryProps}>
+              {entries.map((entry, index) => (
+                <div
+                  key={index}
+                  className={`entryCard ${
+                    index === currentIndex ? "visible" : ""
+                  }`}
+                >
+                  <li>
+                    <p>Date: {entry.date}</p>
+                    <p>Content: {entry.content}</p>
+                    <p>Time: {entry.currentTime}</p>
+                    <p>Author: {entry.author || "Anonymous"}</p>
+                    <button onClick={() => handleDeleteEntry(index)}>
+                      Delete Entry
+                    </button>
+                  </li>
+                </div>
+              ))}
+            </animated.div>
+          ))}
+        </div>
       </ol>
+      <div className="navigationButtons">
+        <button onClick={handlePrev} disabled={currentIndex === 0}>
+          Previous
+        </button>
+        <button
+          onClick={handleNext}
+          disabled={currentIndex === entries.length - 1}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 };
