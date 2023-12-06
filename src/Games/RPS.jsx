c087fcceb498405bbd27fc8944010ffdfbbb9d6a
anickacodes <anickalewis@pursuit.org>
2023-12-05 20:47:11 -0500

    state added

--- src/Games/RPS.jsx
+++ src/Games/RPS.jsx
@@ -1,49 +1,66 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const RPS = () => {
+
+    const [result, setResult] = useState(null)
   const play = (userChoice) => {
-    const choices = ['rock', 'paper', 'scissors'];
+    
+    const choices = ["rock", "paper", "scissors"];
     const computerChoice = choices[Math.floor(Math.random() * choices.length)];
 
     if (userChoice === computerChoice) {
-      setResult('It\'s a draw!');
+      console.log(`USER: ${userChoice} VS CPU: ${computerChoice}`);
+      setResult("It's a draw!");
     } else if (
-      (userChoice === 'rock' && computerChoice === 'scissors') ||
-      (userChoice === 'paper' && computerChoice === 'rock') ||
-      (userChoice === 'scissors' && computerChoice === 'paper')
+      (userChoice === "rock" && computerChoice === "scissors") ||
+      (userChoice === "paper" && computerChoice === "rock") ||
+      (userChoice === "scissors" && computerChoice === "paper")
     ) {
-      setResult('You win!');
+      console.log(`USER: ${userChoice} VS CPU: ${computerChoice}`);
+      setResult("You win!");
     } else {
-      setResult('You lose!');
+      console.log(`USER: ${userChoice} VS CPU: ${computerChoice}`);
+      setResult("You lose!");
     }
-  };
-
-  const setResult = (message) => {
-    document.getElementById('result').textContent = message;
   };
 
   return (
     <div>
       <h1>Rock, Paper, Scissors</h1>
-      <button style={{
-              fontSize: "18px",
-              margin: "10px",
-              padding: "10px",
-              cursor: "pointer",
-            }} onClick={() => play('rock')} >Rock</button>
-      <button style={{
-              fontSize: "18px",
-              margin: "10px",
-              padding: "10px",
-              cursor: "pointer",
-            }} onClick={() => play('paper')}>Paper</button>
-      <button style={{
-              fontSize: "18px",
-              margin: "10px",
-              padding: "10px",
-              cursor: "pointer",
-            }} onClick={() => play('scissors')}>Scissors</button>
-      <p id="result"></p>
+      <button
+        style={{
+          fontSize: "18px",
+          margin: "10px",
+          padding: "10px",
+          cursor: "pointer",
+        }}
+        onClick={() => play("rock")}
+      >
+        Rock
+      </button>
+      <button
+        style={{
+          fontSize: "18px",
+          margin: "10px",
+          padding: "10px",
+          cursor: "pointer",
+        }}
+        onClick={() => play("paper")}
+      >
+        Paper
+      </button>
+      <button
+        style={{
+          fontSize: "18px",
+          margin: "10px",
+          padding: "10px",
+          cursor: "pointer",
+        }}
+        onClick={() => play("scissors")}
+      >
+        Scissors
+      </button>
+      <p id="result">{result}</p>
     </div>
   );
 };
