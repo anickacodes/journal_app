a97f1be8e9250a2d00a4c8c4f0f208f5eaf31a48
anickacodes <anickalewis@pursuit.org>
2023-12-14 02:20:42 -0500

    register access

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import EntryList from "./components/EntryList";
 import Home from "./components/Home";
@@ -11,15 +11,20 @@ import ResetPassword from "./components/ResetPassword";
 
 function App() {
   const [user, setUser] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const jwtToken = localStorage.getItem("jwtToken");
     if (jwtToken) {
-      setUser({ token: jwtToken })
+      setUser({ token: jwtToken });
+      console.log("Token retrieved from localStorage:", jwtToken);
+    } else {
+      navigate("/");
     }
-  }, [])
+  }, []);
 
   const handleLogin = (userData) => {
+    console.log("Received token during login:", userData);
     setUser(userData);
     localStorage.setItem("jwtToken", userData.token);
   };
@@ -31,24 +36,21 @@ function App() {
 
   return (
     <>
-        <NavBar user={user} onLogout={handleLogout} />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              user ? <Navigate to="/home" /> : <LogIn onLogin={handleLogin} />
-            }
-          />
-          <Route path="/home" element={<Home />} />
-          <Route path="/new" element={<Entry user={user} />} />
-          <Route path="/list" element={<EntryList user={user} />} />
-          <Route
-            path="/users/login"
-            element={<LogIn onLogin={handleLogin} />}
-          />
-          <Route path="/users/register" element={<Register />} />
-          <Route path="/users/reset-password" element={<ResetPassword />} />
-        </Routes>
+      <NavBar user={user} onLogout={handleLogout} />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            user ? <Navigate to="/home" /> : <LogIn onLogin={handleLogin} />
+          }
+        />
+        <Route path="/home" element={<Home />} />
+        <Route path="/new" element={<Entry user={user} />} />
+        <Route path="/list" element={<EntryList user={user} />} />
+        <Route path="/users/login" element={<LogIn onLogin={handleLogin} />} />
+        <Route path="/users/register" element={<Register />} />
+        <Route path="/users/reset-password" element={<ResetPassword />} />
+      </Routes>
     </>
   );
 }
