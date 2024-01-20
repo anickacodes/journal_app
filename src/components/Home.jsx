4dcb91f0f56b48bcc8b2896c47c2ca273e878ab8
anickacodes <anickalewis@pursuit.org>
2024-01-20 16:25:15 -0500

    links=>buttons + rm fluff

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,20 @@
+import { Link } from "react-router-dom";
 import "../styles/Home.css";
 
 const Home = () => {
   return (
-    <>
+    <div className="home">
       <div className="home_container">
         <div className="intro">
-          This site is currently under construction
+          <p>We are currently conducting maintenance on the site</p>
+          <p>As we work to create a smoother user experience</p>
+          <p>Please feel free to browse through the app </p>
           <p style={{ color: "violet" }}>
-            Feel free to login/register & browse around
-          </p>
-          This site is currently under construction
-          <p style={{ color: "violet" }}>
-            Or play a game from the options below
+            Or click <Link to="/games"> here </Link> to play a game
           </p>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
